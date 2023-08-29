136f03335a0e45f59f1c2fa4579746693517af67
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2023-08-29 18:11:55 -0500

    player moves

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ let rightPressed = false;
 let upPressed = false;
 let downPressed = false;
 
+let connected = false
+
 function setUpCanvas(){ // sets default player attributes
     canvas.width = window.innerWidth;       // Example Width 1000px
     canvas.height = window.innerHeight;     // Example Height 1000px
@@ -30,12 +32,12 @@ setUpCanvas();
 window.addEventListener('resize', setUpCanvas); // listens for window resize
 
 window.addEventListener('gamepadconnected', (event) => {    // gamepad Connected event listener. Must press button first.
-    console.log('gamepad connected');
+    connected = true
     controllerIndex = event.gamepad.index;
 })
 
 window.addEventListener('gamepaddisconnected', (event) => {    // gamepad Disconnected event listener
-    console.log('gamepad DISconnected');
+    connected = false
     controllerIndex = event.gamepad.index;
 })
 
@@ -52,14 +54,69 @@ function drawPlayer(){
 function checkPlayerAttributes (){
     console.log('player W/H: ', playerWidthAndHeight, "x", playerWidthAndHeight);
     console.log('playerX: ', playerX, ". ", "playerY: ", playerY);
-
+    console.log('gamepad Connected Status: ', connected);
+    // console.log(gamepad.controllerIndex);
+    // console.log(gamepad.buttons);
 }
+
+function controllerInput() {
+    if(controllerIndex !== null) {
+        const gamepad = navigator.getGamepads()[controllerIndex]
+        const buttons = gamepad.buttons;
+        upPressed = buttons[12].pressed;
+        downPressed = buttons[13].pressed;
+        leftPressed = buttons[14].pressed;
+        rightPressed = buttons[15].pressed;
+
+        const stickDeadZone = 0.4;
+        const leftRightValue = gamepad.axes[0];
+        const upDownValue = gamepad.axes[1];
+
+        if(leftRightValue >= stickDeadZone) {
+            rightPressed = true;
+        } 
+        else if (leftRightValue <= -stickDeadZone) {
+            leftPressed = true;
+        }
+
+        if(upDownValue >= stickDeadZone) { // if gamepad up/down axes is >= than deadZone, move up
+            upPressed = true;
+        } 
+        else if (upDownValue <= -stickDeadZone) { // if gamepad up/down axes is <= than deadZone, move down
+            downPressed = true;
+        }
+
+    }
+};
     
+function movePlayer() {
+    if(upPressed) {
+        playerY -= velocity;
+    }
+    if(downPressed) {
+        playerY += velocity;
+    }
+    if(leftPressed) {
+        playerX -= velocity;
+    }
+    if(rightPressed) {
+        playerX += velocity;
+    }
+}
+
+function updatePlayer() {
+    movePlayer();
+    //changePlayerColor();
+}
+
 function gameLoop() { 
-    console.log('gameLoop');
+    // console.log('gameLoop');
     clearScreen();  // clears the screen. Sets dark grey screen
-    drawPlayer();   // 
+    drawPlayer();   
+    controllerInput();
+    updatePlayer();
     requestAnimationFrame(gameLoop); // recursion
+    // checkPlayerAttributes();     // Checks/Console logs gamepad attributes
 }
 
 gameLoop();
